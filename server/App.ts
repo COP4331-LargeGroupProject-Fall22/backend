11c895ab7fd5314cd788910c503237fb8c5eef24
Michael Plekunov <mplekunov@gmail.com>
2022-10-23 13:22:07 -0400

    Heroku

--- server/App.ts
+++ server/App.ts
@@ -2,42 +2,39 @@
  * Entry point of the Food API.
  * This file is responsible for setting up server and routing api request to their respective routes.
  */
- import * as dotenv from 'dotenv';
- dotenv.config();
- 
- import express from 'express';
- import path from 'path';
- 
- import Logger from './serverAPI/middleware/logger/Logger';
- import { userRoute } from './serverAPI/routes/UserRoutes';
- import { authenticationRoute } from './serverAPI/routes/AuthenticationRoutes';
- import { foodRoute } from './serverAPI/routes/FoodRoute';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
- const app = express();
- 
- app.use(Logger.consoleLog);
+import express from 'express';
+import path from 'path';
 
- if (process.env.NODE_ENV === 'production') {
+import Logger from './serverAPI/middleware/logger/Logger';
+import { userRoute } from './serverAPI/routes/UserRoutes';
+import { authenticationRoute } from './serverAPI/routes/AuthenticationRoutes';
+import { foodRoute } from './serverAPI/routes/FoodRoute';
+
+const app = express();
+
+app.use(Logger.consoleLog);
+
+if (process.env.NODE_ENV === 'production') {
     // app.use(express.static('frontend/build'));
     app.use(express.static(path.resolve(__dirname, './serverAPI/view/html/public')));
 
-    app.get('*', (req,res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './serverAPI/view/html/public/index.html'));
     });
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, './server', 'build', 'index.html'));
-    // })
- }
- 
- app.use('/users', userRoute);
- app.use('/foods', foodRoute);
- app.use('/auth', authenticationRoute);
 
- const server = (port: number) => {
+    app.use('/users', userRoute);
+    app.use('/foods', foodRoute);
+    app.use('/auth', authenticationRoute);
+}
+
+const server = (port: number) => {
     app.listen(port, () => {
         console.log(`🚀 Server is running on port ${port}`);
     });
 }
 
- export { app, server };
+export { app, server };
 
