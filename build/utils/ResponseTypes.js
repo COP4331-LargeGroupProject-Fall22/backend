fd82428b232f867d62690a85002c777768b0c2ab
Michael <38502074+mplekunov@users.noreply.github.com>
2022-10-25 15:42:32 -0400

    Upd: remove build/

--- build/utils/ResponseTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResponseTypes = void 0;
-var ResponseTypes;
-(function (ResponseTypes) {
-    ResponseTypes[ResponseTypes["ERROR"] = 0] = "ERROR";
-    ResponseTypes[ResponseTypes["SUCCESS"] = 1] = "SUCCESS";
-})(ResponseTypes = exports.ResponseTypes || (exports.ResponseTypes = {}));
-//# sourceMappingURL=ResponseTypes.js.map
