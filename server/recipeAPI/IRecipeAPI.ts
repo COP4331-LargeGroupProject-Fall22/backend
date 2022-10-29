e27b74946dbe2e45a7092eafd98c826378d4c2ae
Michael Plekunov <mplekunov@gmail.com>
2022-10-29 14:11:48 -0400

    Upd: addressed comments

--- server/recipeAPI/IRecipeAPI.ts
+++ server/recipeAPI/IRecipeAPI.ts
@@ -5,13 +5,12 @@ export default interface IRecipeAPI {
      * Retrieves collection of partially filled Recipe items that satisfy searching parameters.
      * 
      * @param parameters parameters used for searching.
-     * - query - required parameter that defines the name of the Recipe item (partial names are accepted).
+     * - recipeName - required parameter that defines the name of the Recipe item (partial names are accepted).
      * - number - optional parameter that defines max number of the results to be returned.
      * - page - optional parameter that defines page number for pagination.
-     * - intolerence - optional parameter that defines the type of intolerences to be taken in consideration during searching.
+     * - intolerance - optional parameter that defines the type of intolerances to be taken in consideration during searching.
      * 
      * @throws NoParameterFound exception when required parameters weren't found.
-     *
      * @returns Promise filled with a collection of Partial<IRecipe> objects.
      */
     SearchRecipe(parameters: Map<string, any>): Promise<Partial<IRecipe>[]>;
@@ -23,7 +22,6 @@ export default interface IRecipeAPI {
      * - id - required parameter that defines unique identifier of the Recipe item
      * 
      * @throws NoParameterFound exception when required parameters weren't found.
-     *
      * @returns Promise filled with a IRecipe object or null when Recipe item wasn't found.
      */
     GetRecipe(parameters: Map<string, any>): Promise<IRecipe | null>;
