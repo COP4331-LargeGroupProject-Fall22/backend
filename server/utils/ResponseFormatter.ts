47d8be2a4bb4029d00ed8b4ed817de2653559bc9
Michael Plekunov <mplekunov@gmail.com>
2022-11-12 16:27:36 -0500

    Add: response codes

--- server/utils/ResponseFormatter.ts
+++ server/utils/ResponseFormatter.ts
@@ -4,7 +4,7 @@ import { ResponseTypes } from "./ResponseTypes";
  * This class is responsible for providing implementation for formatting depending on different resonse types.
  */
 export default class ResponseFormatter {
-    static formatAsJSON(type: ResponseTypes, data: any = null): any {
+    static formatAsJSON(type: ResponseTypes, data: any = {}): any {
         switch (type) {
             case ResponseTypes.ERROR:
                 return data;
